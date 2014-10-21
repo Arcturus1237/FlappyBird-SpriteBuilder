ebbe43d13d62290e70ebf4e79f6719f3dccb5536
Arcturus1237 <arcturus1237@icloud.com>
2014-10-21 14:44:16 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -16,7 +16,7 @@
 
 -(void)update:(CCTime)delta
 {
-    // this will be run every frame.
+    /// this will be run every frame.
     // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
 }
 
