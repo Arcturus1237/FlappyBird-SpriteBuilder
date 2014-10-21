7180d105ba98edc39f5e3f0aeacdf312d0148d89
Arcturus1237 <arcturus1237@icloud.com>
2014-10-21 14:54:41 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -16,8 +16,18 @@
 
 -(void)update:(CCTime)delta
 {
-    /// this will be run every frame.
-    // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+    // put update code here
+    // increment the time since the last obstance was added
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
+    
+    if (timeSinceObstacle > 2.0f)
+        {
+            //Add a new obstacle
+            [self addObstacle];
+            
+            //Then reset the timer
+            timeSinceObstacle = 0.0f;
+            }
 }
 
 // put new methods here
